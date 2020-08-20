287ad3f6f4c75afeddae456b420e5e8e42ba9474
slow999 <slow999@gmail.com>
2020-08-20 13:07:44 -0500

    further improvement on printing

--- home/signals.py
+++ home/signals.py
@@ -4,14 +4,14 @@ from django.contrib.auth.signals import user_logged_in, user_logged_out, user_lo
 
 @receiver(user_logged_in)
 def log_user_login(sender, request, user, **kwargs):
-    print('user logged in')
+    print('user {} logged in through page {}'.format(user.username, request.META.get('HTTP_REFERER')))
 
 
 @receiver(user_login_failed)
 def log_user_login_failed(sender, credentials, request, **kwargs):
-    print('user logged in failed')
+    print('user {} logged in failed through page {}'.format(credentials.get('username'), request.META.get('HTTP_REFERER')))
 
 
 @receiver(user_logged_out)
 def log_user_logout(sender, request, user, **kwargs):
-    print('user logged out')
+    print('user {} logged out through page {}'.format(user.username, request.META.get('HTTP_REFERER')))
